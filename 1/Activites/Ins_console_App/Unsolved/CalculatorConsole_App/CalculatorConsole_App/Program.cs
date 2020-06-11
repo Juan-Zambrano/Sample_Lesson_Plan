a4e1a66a3fd973a1aa95e4b680d91c4c08ed5161
Juan Zambrano <Juanzambrano36@gmail.com>
2020-06-10 23:20:40 -0500

    console app in solution

---
+++ 1/Activites/Ins_console_App/Unsolved/CalculatorConsole_App/CalculatorConsole_App/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CalculatorConsole_App
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //Since first lecture we are not covering exception handeling.
+
+            Console.WriteLine("Simple Calculator App");
+            Console.WriteLine("Enter large number:");
+            Double input_one = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter small number:");
+            Double input_two = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Add or Subtract");
+            string operation = Console.ReadLine();
+            Double retval = 0;
+
+            if (operation == "add")
+            {
+                retval = input_one + input_two;
+            }
+            if (operation == "subtract")
+            {
+                retval = input_one - input_two;
+            }
+        
+            Console.WriteLine("Calculated amount:" + Convert.ToString(retval));
+            Console.ReadKey();
+        }
+
+    }
+}
